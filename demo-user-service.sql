bffeb57cd5956f65746ead9b2e595ed30c1e8fb7
hand_wenqiang.chen <649418826@qq.com>
2022-11-25 09:39:53 +0800

    更新代码

--- demo-user-service.sql
+++ demo-user-service.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80029 (8.0.29)
  File Encoding         : 65001
 
- Date: 23/11/2022 17:47:25
+ Date: 24/11/2022 18:55:58
 */
 
 SET NAMES utf8mb4;
@@ -22,14 +22,14 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` bigint NOT NULL COMMENT '用户id',
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户id',
   `nick_name` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户昵称',
   `sex` char(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '性别',
-  `age` int NULL DEFAULT NULL COMMENT '出生日期',
-  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
+  `age` int NULL DEFAULT NULL COMMENT '年龄',
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '手机号',
   `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '所在地址',
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '账户密码',
-  `icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '账户头像',
+  `icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '账户头像',
   `create_time` datetime NULL DEFAULT NULL COMMENT '用户注册时间',
   `update_time` datetime NULL DEFAULT NULL COMMENT '信息更新时间',
   PRIMARY KEY (`id`) USING BTREE
@@ -38,6 +38,6 @@ CREATE TABLE `user`  (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, '萧炎', '男', 18, '18888888888', '迦南学院', NULL, NULL, NULL, NULL);
+INSERT INTO `user` VALUES ('demo340c46980b894b0ba6f5d02d17234026', '萧炎', '男', 18, '18888888888', '加玛帝国', 'qwe123', 'https://img1.baidu.com/it/u=1881729541,3170519255&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1669395600&t=e94232e66479c36b8232f62408a18ff9', '2022-11-24 18:53:30', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
