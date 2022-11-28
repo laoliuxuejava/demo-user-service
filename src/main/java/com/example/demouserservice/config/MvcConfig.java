2837c85527723e818b1d20a42ec684998ae956ac
hand_wenqiang.chen <649418826@qq.com>
2022-11-28 17:57:29 +0800

    更新代码

---
+++ src/main/java/com/example/demouserservice/config/MvcConfig.java
@@ -0,0 +1,25 @@
+package com.example.demouserservice.config;
+
+import com.example.demouserservice.utils.LoginInterceptor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author guser
+ * @Date 2022/11/28
+ */
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor(stringRedisTemplate)).
+                excludePathPatterns("" +
+                        "/user/**");
+    }
+}
---
+++ src/main/java/com/example/demouserservice/config/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+package com.example.demouserservice.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author guser
+ * @Date 2022/11/25
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    //配置mybatisPlus分页拦截器
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //添加分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        //添加乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+}
